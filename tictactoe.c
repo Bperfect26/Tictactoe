4d193a38e18d5217b6771635844e14868b3f9e3a
Bperfect26 <benperfect26@gmail.com>
2025-11-13 20:06:22 -0500

    chat2

---
+++ tictactoe.c
@@ -0,0 +1,11 @@
+// Include necessary headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX_SIZE 10 // Maximum grid size
+void boardinit(){
+    int board[3][3];
+    int row[3];
+    int col[3];
+
+}
